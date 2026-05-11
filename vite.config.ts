f0d58f8828c1e959a2769565c5384944fee8ca67
HungDev <baohungdinhnguyen1412@gmail.com>
2026-05-11 17:32:54 +0700

    Config: ENV

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: process.env.VITE_API_BASE_URL || 'http://localhost:8080',
+        target: process.env.VITE_API_BASE_URL || 'http://localhost:7070',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
